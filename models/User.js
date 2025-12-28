ea5e2f4ddf2cce3e0534da2c69bf392af7ca7ce3
JatinChhikara-28 <chhikarajatin96@gmail.com>
2025-12-28 14:12:11 +0530

    First Commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose, { trusted } from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        _id:{type : string , required : true},
+        name : { type:string , required: true},
+        email : {type:string , required: true, unique: true},
+        imageUrl : {type:string , required: true},
+        cartItems : {type:object , default: {}},
+    },
+    {minimize : false}
+) 
+
+const User = mongoose.models.user || mongoose.model(`user` , userSchema)
+
+export default User
