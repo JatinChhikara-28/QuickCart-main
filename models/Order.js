5354a9ab2d589638f58e96fd5fb42c70d33da6cf
JatinChhikara-28 <chhikarajatin96@gmail.com>
2025-12-31 14:59:49 +0530

    sixth commit

--- models/Order.js
+++ models/Order.js
@@ -9,7 +9,7 @@ const orderSchema = new mongoose.Schema({
     }],
     amount:{type:Number,required:true},
     address:{type:String,required:true,ref:'address'},
-    status:{tyoe:String,required:true,default:'Order Placed'},
+    status:{type:String,required:true,default:'Order Placed'},
     date:{type:Number,required:true},
 })
 
